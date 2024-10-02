64c4b92908886fa36eaf915ca59e959d5997f910
Kerolay <hana.kerolay@gmail.com>
2024-10-02 17:26:15 -0300

    subiralteracao

--- js/script.js
+++ js/script.js
@@ -15,30 +15,30 @@ function segundograu() {
 
 
 function calcularHipotenusa() {
-    // Pegando os valores dos catetos
+    
     let catetoOpos = Number(document.getElementById("catetoOpos").value);
     let catetoAdjacen = Number(document.getElementById("catetoAdjacen").value);
 
-    // Calculando a hipotenusa usando o Teorema de Pitágoras
+    
     let hipotenusa = Math.sqrt(Math.pow(catetoOpos, 2) + Math.pow(catetoAdjacen, 2));
 
-    // Exibindo o resultado
+    
     document.getElementById("resultado").innerText = `A hipotenusa é: ${hipotenusa.toFixed(2)}`;
 }
 
 
 function calcularRelacTrigonomet() {
-    // Pegando os valores dos catetos e da hipotenusa
+    
     let catetoOposto = parseFloat(document.getElementById("catetoOposto").value);
     let catetoAdjacente = parseFloat(document.getElementById("catetoAdjacente").value);
     let hipotenusa = parseFloat(document.getElementById("hipotenusa").value);
 
-    // Calculando as relações trigonométricas
+    
     let seno = catetoOposto / hipotenusa;
     let cosseno = catetoAdjacente / hipotenusa;
     let tangente = catetoOposto / catetoAdjacente;
 
-    // Exibindo os resultados
+    
     document.getElementById("resultadoSeno").innerText = `Seno: ${seno.toFixed(4)}`;
     document.getElementById("resultadoCosseno").innerText = `Cosseno: ${cosseno.toFixed(4)}`;
     document.getElementById("resultadoTangente").innerText = `Tangente: ${tangente.toFixed(4)}`;
